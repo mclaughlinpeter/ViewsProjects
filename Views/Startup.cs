c05861ec4ef7ed96fcc7a5edb3882231351d9be1
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2018-02-10 08:51:18 -0500

    Register DebugDataViewEngine in Startup class

--- Views/Startup.cs
+++ Views/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Views.Infrastructure;
 
 namespace Views
 {
@@ -14,6 +16,9 @@ namespace Views
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.Configure<MvcViewOptions>(options => {
+                options.ViewEngines.Insert(0, new DebugDataViewEngine());
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
